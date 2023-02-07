0b29704d05ea8abf703d6aec88db05ac4a9f4064
Marco <113130222+MarcoDeMelo@users.noreply.github.com>
2023-02-07 15:03:09 -0500

    Add files via upload

---
+++ V2/V2(MarcoDeMelo)/Footer.js
@@ -0,0 +1,14 @@
+
+
+function todaysDate()
+{
+	
+
+	var dt = new Date();
+document.getElementById('Date').innerHTML=dt;
+}
+function changeColour() {
+	document.getElementById("changeColour").style.border = "0.3em  dotted yellow";
+
+	
+  }
